19182e399be00601fb2f192f48e0c4610e6fb78c
Aurora Esmeralda <auroraesmeralda@protonmail.com>
2025-05-29 12:27:36 +1000

    Add username filtering as a toggleable option

--- lib/features.rb
+++ lib/features.rb
@@ -2,4 +2,12 @@ module Features
   def self.discord_integration_enabled?
     Rails.configuration.features[:discord_integration]
   end
+
+  def self.name_filtering_enabled?
+    Rails.configuration.features[:name_filtering][:enabled]
+  end
+
+  def self.name_filter
+    Regexp.new Rails.configuration.features[:name_filtering][:filter]
+  end
 end
